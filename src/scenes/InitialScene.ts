3a9c63086bba09d7446f991bbb86215695304e2b
Paula Santamaría <m_paula_santamaria@live.com>
2021-11-29 01:10:24 -0300

    Lint fixes

--- src/scenes/InitialScene.ts
+++ src/scenes/InitialScene.ts
@@ -14,9 +14,9 @@ export default class InitialScene extends Phaser.Scene {
         this.add.image(400, 570, 'ground');
         this.hero = this.physics.add.image(0, 0, 'hero');
 
-        console.info('Contributor: ' + Contributor.username)
-        console.info('Commit: ' + Contributor.commit)
-        console.info('Commig message: ' + Contributor.message)
+        console.info('Contributor: ' + Contributor.username);
+        console.info('Commit: ' + Contributor.commit);
+        console.info('Commig message: ' + Contributor.message);
         
         this.hero.setCollideWorldBounds(true);
         this.hero.setBounce(1, 1);
