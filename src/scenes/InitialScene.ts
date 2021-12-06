954eb56d1e5b13902799a8e5d77c53df14359c5e
paulasantamaria <m_paula_santamaria@live.com>
2021-12-06 09:56:51 -0300

    Add basic character movement

--- src/scenes/InitialScene.ts
+++ src/scenes/InitialScene.ts
@@ -1,9 +1,14 @@
 import Phaser from 'phaser';
-
-import { Contributor } from '../Contributor';
+import ControllerKeys from '../utils/ControllerKeys';
 
 export default class InitialScene extends Phaser.Scene {
-    private hero!: Phaser.Types.Physics.Arcade.ImageWithDynamicBody;
+    private characterSprite!: Phaser.Types.Physics.Arcade.ImageWithDynamicBody;
+
+    private controllerKeys!: ControllerKeys;
+
+    private characterSpeed = 30;
+    private characterJumpSpeed = 30;
+    private characterIsJumping = false;
 
     constructor() {
         super('initial');
@@ -11,15 +16,58 @@ export default class InitialScene extends Phaser.Scene {
 
     create(): void {
 
-        this.add.image(400, 570, 'ground');
-        this.hero = this.physics.add.image(0, 0, 'hero');
+        this.controllerKeys = new ControllerKeys(this, 'wasd');
 
-        console.info('Contributor: ' + Contributor.username);
-        console.info('Commit: ' + Contributor.commit);
-        console.info('Commig message: ' + Contributor.message);
-        
-        this.hero.setCollideWorldBounds(true);
-        this.hero.setBounce(1, 1);
-        this.hero.setVelocityX(300);
+        // Setup floor
+        const floorSprite = this.add.image(0, 0, 'floor');
+        floorSprite.setOrigin(0, 0);
+        floorSprite.setPosition(0, this.cameras.main.height - floorSprite.height);
+
+        // Setup floor body
+        const floorBody = this.add.rectangle(0, this.cameras.main.height - floorSprite.height + 20, floorSprite.width - 20, floorSprite.height - 20);
+        floorBody.setOrigin(0, 0);
+        this.physics.add.existing(floorBody, true);
+        (floorBody.body as Phaser.Physics.Arcade.Body).debugBodyColor = 0x018852;
+
+        // Setup sky
+        const skySprite = this.add.image(0, 0, 'sky');
+        skySprite.setOrigin(0, 0);
+        skySprite.setPosition(0, 0);
+
+        // Setup character
+        this.characterSprite = this.physics.add.image(0, 0, 'character');
+        this.characterSprite.setOrigin(0, 0);
+        this.characterSprite.setCollideWorldBounds(true);
+        this.characterSprite.setPosition(0, (this.cameras.main.height - this.characterSprite.height) / 2);
+        this.characterSprite.setBounce(0, 0.5);
+        this.characterSprite.setMaxVelocity(500, undefined);
+        this.characterSprite.setVelocityX(this.characterSpeed)
+        this.physics.add.collider(this.characterSprite, floorBody, this.onCharacterCollidesWithFloor, undefined, this);
+    }
+
+    onCharacterCollidesWithFloor(go1: Phaser.GameObjects.GameObject, go2: Phaser.GameObjects.GameObject) {
+        this.characterIsJumping = false;
+    }
+
+    update(time: number, delta: number) {
+        super.update(time, delta);
+
+        // Character controller
+        let speed = { x: 0, y: 0 };
+
+        if (this.controllerKeys.left.isDown)
+            speed.x = -1 * this.characterSpeed * delta;
+
+        else if (this.controllerKeys.right.isDown)
+            speed.x = 1 * this.characterSpeed * delta;
+
+        if (this.controllerKeys.jump.isDown && !this.characterIsJumping) {
+            this.characterIsJumping = true;
+            speed.y = -1 * this.characterJumpSpeed * delta;
+
+            this.characterSprite.setVelocityY(speed.y);
+        }
+
+        this.characterSprite.setAccelerationX(speed.x);
     }
 }
