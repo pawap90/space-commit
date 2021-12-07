d7fbe201e68e64274559235119fad4126d09c1f6
Paula Santamaría <m_paula_santamaria@live.com>
2021-12-07 16:20:21 -0300

    Add contributor file to prevent errors on ci

---
+++ src/Contributor.ts
@@ -0,0 +1,6 @@
+export const Contributor = {
+    username: 'pawap90',
+    avatar_url: 'https://avatars.githubusercontent.com/u/2507959?v=4',
+    commit: '1234abcd',
+    message: 'Test message'
+};
