d3f0b9edd8e0eb806db388bf38262e09a202616e
Paula Santamaría <m_paula_santamaria@live.com>
2021-12-08 15:08:57 -0300

    Load every asset in preloader

--- src/scenes/PreloaderScene.ts
+++ src/scenes/PreloaderScene.ts
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import { Contributor } from '../Contributor';
 
 export default class Preloader extends Phaser.Scene {
     constructor() {
@@ -11,10 +12,13 @@ export default class Preloader extends Phaser.Scene {
         this.load.image('astronaut', 'assets/astronaut.png');
         this.load.image('enemy', 'assets/enemy.png');
         
+        this.load.image('logo', 'assets/logo.png');
+        this.load.image('avatar', Contributor.avatar_url);
         this.load.image('avatar-mask', 'assets/avatar-mask.png');
+        this.load.image('menu-avatar-mask', 'assets/menu-avatar-mask.png');
     }
 
     create(): void {
-        this.scene.start('initial');
+        this.scene.start('main-menu');
     }
 }
